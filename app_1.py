e3b56006ac5fe732ef0568e9b4096d1db94a6718
njiutheguru <stephennjiu@gmail.com>
2024-07-05 09:00:18 +0300

    chat history enabled

--- app_1.py
+++ app_1.py
@@ -1,8 +1,5 @@
 from flask import Flask, request, render_template, jsonify
 import markdown
-    # markdown.markdown()
-
-
 
 app = Flask(__name__)
 # define the open_api_key
@@ -10,13 +7,15 @@ app = Flask(__name__)
 from openai import OpenAI
 client = OpenAI(api_key= OPENAI_API_KEY)
 assistant = client.beta.assistants.create(
-    name="medical Assistant",
+    name="health Assistant",
     instructions="You are a useful medical assistant. Answer and chat with patients and answer questions or concerns which are ONLY RELATED TO HEALTH OR MEDICINE!.",
     tools=[{"type": "code_interpreter"}],
     model="gpt-4o")
+
 thread = client.beta.threads.create()
 
-def chat(prompt):
+
+def get_ai_response(prompt):
   message = client.beta.threads.messages.create(
     thread_id=thread.id,
     role="user",
@@ -26,7 +25,6 @@ def chat(prompt):
     thread_id=thread.id,
     assistant_id=assistant.id,
     instructions="Only answer questions about 'health','medicine', or 'mental health', else  you must return 'I am sorry I don't know about that.I only deal with medicine and health issues. Please contact the customer care for more direction'.BE BRIEF."
-
   )
   if run.status == 'completed': 
     messages = client.beta.threads.messages.list(
@@ -39,15 +37,31 @@ def chat(prompt):
 
 def chatbot_response(user_input):
     # Replace this with your actual chatbot logic
-    return f"Zoe: {markdown.markdown(chat(user_input))}"
+    response = f"{markdown.markdown(get_ai_response(user_input))}"
+    return response
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/', methods=['GET'])
 def index():
-    if request.method == 'POST':
-        user_input = request.form['user_input']
-        bot_response = chatbot_response(user_input)
-        return render_template('index.html', user_input=user_input, bot_response=bot_response)
-    return render_template('index.html')
+    return render_template('bot.html')
+
+
+@app.route('/chat', methods=['POST'])
+def chat():
+   user_input = request.json['user_input']
+   bot_response = chatbot_response(user_input)
+   return jsonify({
+        'user_input': user_input,
+        'bot_response': bot_response
+    })
+
+   
+
+    # if request.method == 'POST':
+    #     user_input = request.form['user_input']
+    #     bot_response = chatbot_response(user_input)
+    #     return render_template('index.html', user_input=user_input, bot_response=bot_response)
+    # return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
