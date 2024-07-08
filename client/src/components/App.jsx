d4d03a3fc289e6c0dd42c20470919b5a2f6ef0dd
Adrian Chege <achege116@gmail.com>
2024-07-09 01:57:06 +0300

    Routes to the login and signup page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,6 +4,8 @@ import HomePage from "./Home/home";
 import Booking from "./Appointment/appointment";
 import ContactPage from "./Contact/contact,";
 import Chatbot from "./Chatbot/chatbot";
+import SignUpPage from "./Signup/signup";
+import LoginPage from "./Login/login";
 
 function App() {
   
@@ -12,6 +14,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/chatbot" element={<Chatbot />} />
         <Route path="/services" element={<Booking />} />
