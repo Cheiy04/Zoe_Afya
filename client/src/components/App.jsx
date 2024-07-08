2da6ab96a9e8c00b0bd15dd8b6165f725f2c8531
Adrian Chege <achege116@gmail.com>
2024-07-08 15:51:21 +0300

    Added the contact page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import WelcomePage from "./Index/welcome";
 import HomePage from "./Home/home";
 import Booking from "./Appointment/appointment";
+import ContactPage from "./Contact/contact,";
 
 function App() {
   
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<WelcomePage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/services" element={<Booking />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
 
     </Router>
