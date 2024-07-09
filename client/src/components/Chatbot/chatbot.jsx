1b93ab32720d71c311031a551b8f13c607640f26
Adrian Chege <achege116@gmail.com>
2024-07-09 22:47:49 +0300

    Changes to chatbot

--- client/src/components/Chatbot/chatbot.jsx
+++ client/src/components/Chatbot/chatbot.jsx
@@ -7,7 +7,6 @@ const Chatbot = () => {
   return (
     <div className="chatbot-container">
       <Navbar />  
-      <h2>Zoe-Bot</h2>
       <iframe
         src="https://brave-doralia-njiu-373920ef.koyeb.app/" // Your deployed URL
         title="Chatbot"
